9b7bc8416ad076dc317f2839c190a00d84c5e267
Viachaslau Shpileuski <viachaslau.shpileuski@coxautoinc.com>
2022-11-02 00:00:17 +0300

    feat: complete task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://giur5atxuf.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://06jrxjer49.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
